e5619f0a5de89cf02981e0642cbe2dbd564a33a6
md sinikdho <mdsinikdho94@gmail.com>
2026-04-18 21:29:34 +0600

    Create Store or Config store and connect out react app using provider component

--- src/features/counters/countersSlice.js
+++ src/features/counters/countersSlice.js
@@ -1,12 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
-import initialCounters from "../../data/initialCounters";
-const counterSlice = createSlice({
-    name: "counters",
-    initialState : initialCounters,
-    reducers:{
-        increment (sate,action) =>{
-            
-        },
-        decrement (sate,action) =>{},
-    }
-})
+import initialCounters from "./../../data/initialCounters";
+const CounterSlice = createSlice({
+  name: "counters",
+  initialState: initialCounters,
+  reducers: {
+    increment(sate, action) {
+      const counterIndex = sate.findIndex((c) => c.id === action.payload);
+      sate[counterIndex].value++;
+    },
+    decrement(sate, action) {
+      const counterIndex = sate.findIndex((c) => c.id === action.payload);
+      sate[counterIndex].value--;
+    },
+  },
+});
+
+export default CounterSlice.reducer;
+export const { increment, decrement } = CounterSlice.actions;
